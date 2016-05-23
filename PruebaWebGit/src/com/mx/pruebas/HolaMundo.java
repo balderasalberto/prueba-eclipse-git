5aa1ba712ee51ae3c92ecda9fd97b15e675526f3
abalderas <balderas.mexico2011@gmail.com>
2016-05-23 13:29:00 -0500

    Pruebas.

---
+++ PruebaWebGit/src/com/mx/pruebas/HolaMundo.java
@@ -0,0 +1,13 @@
+package com.mx.pruebas;
+
+public class HolaMundo {
+	/**
+	 * Element-value pairs
+	 */
+	public static void main(String[] args) {
+		System.out.println("1234567890");
+		// TODO Auto-generated method stub
+
+	}
+
+}
